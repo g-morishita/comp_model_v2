88c24cfa58abac4954ef762884a61101574bb48c
Gota Morishita <optimistic.gota@gmail.com>
2026-03-11 18:37:43 +1100

    feat(inference): add config and dispatch

--- src/comp_model/inference/__init__.py
+++ src/comp_model/inference/__init__.py
@@ -1,1 +1,6 @@
 """Inference backends and configuration."""
+
+from comp_model.inference.config import HierarchyStructure, InferenceConfig
+from comp_model.inference.dispatch import fit
+
+__all__ = ["HierarchyStructure", "InferenceConfig", "fit"]
